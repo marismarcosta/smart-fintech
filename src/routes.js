13b47eef3ecdeaf8e97c58486ffcf3daf72345b2
EraldoCi <gustavosilva754@gmail.com>
2020-11-14 11:13:46 -0300

    Financial Planning home page

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import BusinessMenu from './pages/BusinessMenu';
 import Planning from './pages/Planning';
 import CreditPlanning from './pages/CreditPlanning'
+import PlannigHome from './pages/PlanningHome';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={BusinessMenu} />
       <Route path="/planning" component={Planning} />
-
+      <Route path="/financial" component={PlannigHome}/>
+      
       <Route path="/credit-planning" component={CreditPlanning} />
     </BrowserRouter>
   );
